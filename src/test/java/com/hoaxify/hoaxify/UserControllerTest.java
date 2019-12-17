b77bfc19bfa66e927d1e1cae7a7e1d2a6bcf1b13
forest-shadow <mamasay@yandex.ru>
2019-12-18 00:06:17 +0300

    Add methods for saving User to DB

--- src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.hoaxify.hoaxify;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,24 +15,48 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hoaxify.hoaxify.user.User;
+import com.hoaxify.hoaxify.user.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class UserControllerTest {
 
+  private static final String API_1_0_USERS = "/api/1.0/users";
+
   @Autowired
   TestRestTemplate testRestTemplate;
 
-  @Test
-  public void postUser_whenUserIsValid_receiveOk() {
+  @Autowired
+  UserRepository userRepository;
+
+  @Before
+  public void cleanup() {
+    userRepository.deleteAll();
+  }
+
+  private User createValidUser() {
     User user = new User();
     user.setUsername("test-user");
     user.setDisplayName("test-display");
     user.setPassword("P4ssword");
 
-    ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
+    return user;
+  }
+
+  @Test
+  public void postUser_whenUserIsValid_receiveOk() {
+    User user = createValidUser();
+
+    ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
+
+  @Test
+  public void postUser_whenUserIsValid_userSavedToDatabase() {
+    User user = createValidUser();
+    testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+    assertThat(userRepository.count()).isEqualTo(1);
   }
 }
